29bbec62a92740f02449a96d74aaec9961b5a881
piyushDSM <piyushchaubeydsm@gmail.com>
2024-03-24 09:59:33 +0530

    updated

--- Array/union2SortedArrray.cpp
+++ Array/union2SortedArrray.cpp
@@ -44,3 +44,49 @@ int main()
     cout << val << " ";
   return 0;
 }
+
+
+/*
+vector < int > sortedArray(vector < int > a, vector < int > b) {
+    // Write your code here
+    int n1 = a.size();
+    int n2 = b.size();
+    int i =0;
+    int j=0;
+
+    vector<int> UnionArr;
+
+    while(i<n1 && j<n2){
+        if(a[i]<=b[j]){
+            if(UnionArr.size()==0 || UnionArr.back()!=a[i]){
+                UnionArr.push_back(a[i]);
+            }
+            i++;
+        }
+
+        else {
+            if(UnionArr.size()==0 || UnionArr.back()!=b[j]){
+                UnionArr.push_back(b[j]);
+            }
+            j++;
+        }
+    }
+
+    while(j<n2){
+         if(UnionArr.size()==0 || UnionArr.back()!=b[j]){
+                UnionArr.push_back(b[j]);
+            }
+            j++;
+    }
+
+    while(i<n1){
+        if(UnionArr.size()==0 || UnionArr.back()!=a[i]){
+                UnionArr.push_back(a[i]);
+            }
+            i++;
+    }
+
+    return UnionArr;
+}
+
+*/
