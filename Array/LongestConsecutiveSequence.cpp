b0dd85c2c1fcf794e03351db2a2a72f2b4e612d0
piyushDSM <piyushchaubeydsm@gmail.com>
2024-04-04 11:25:07 +0530

    Updated

--- Array/LongestConsecutiveSequence.cpp
+++ Array/LongestConsecutiveSequence.cpp
@@ -1,25 +1,31 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int longestSuccessiveElements(vector<int>&a) {
+int longestSuccessiveElements(vector<int> &a)
+{
     int n = a.size();
-    if (n == 0) return 0;
+    if (n == 0)
+        return 0;
 
     int longest = 1;
     unordered_set<int> st;
-    //put all the array elements into set:
-    for (int i = 0; i < n; i++) {
+    // put all the array elements into set:
+    for (int i = 0; i < n; i++)
+    {
         st.insert(a[i]);
     }
 
-    //Find the longest sequence:
-    for (auto it : st) {
-        //if 'it' is a starting number:
-        if (st.find(it - 1) == st.end()) {
-            //find consecutive numbers:
+    // Find the longest sequence:
+    for (auto it : st)
+    {
+        // if 'it' is a starting number:
+        if (st.find(it - 1) == st.end())
+        {
+            // find consecutive numbers:
             int cnt = 1;
             int x = it;
-            while (st.find(x + 1) != st.end()) {
+            while (st.find(x + 1) != st.end())
+            {
                 x = x + 1;
                 cnt = cnt + 1;
             }
@@ -27,7 +33,6 @@ int longestSuccessiveElements(vector<int>&a) {
         }
     }
     return longest;
-
 }
 
 int main()
