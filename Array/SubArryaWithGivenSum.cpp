9409a98fdce1a76fb5a494761145daaa8aefcfc3
piyushDSM <piyushchaubeydsm@gmail.com>
2024-04-05 15:36:18 +0530

    UpdatedQues

---
+++ Array/SubArryaWithGivenSum.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int findAllSubarraysWithGivenSum(vector<int> &arr, int k)
+{
+    int n = arr.size(); // size of the given array.
+    int cnt = 0;        // Number of subarrays:
+
+    for (int i = 0; i < n; i++)
+    { // starting index i
+        int sum = 0;
+        for (int j = i; j < n; j++)
+        { // ending index j
+            // calculate the sum of subarray [i...j]
+            // sum of [i..j-1] + arr[j]
+            sum += arr[j];
+
+            // Increase the count if sum == k:
+            if (sum == k)
+                cnt++;
+        }
+    }
+    return cnt;
+}
+
+int main()
+{
+    vector arr = {3, 1, 2, 4};
+    int k = 6;
+    int cnt = findAllSubarraysWithGivenSum(arr, k);
+    cout << "The number of subarrays is: " << cnt << "\n";
+    return 0;
+}
