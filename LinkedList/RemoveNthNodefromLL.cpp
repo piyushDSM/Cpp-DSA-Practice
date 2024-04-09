de670d26b2f14b5a6569dd099c145438f4228cd7
piyushDSM <piyushchaubeydsm@gmail.com>
2024-04-09 11:26:23 +0530

    UpdatedQues

---
+++ LinkedList/RemoveNthNodefromLL.cpp
@@ -0,0 +1,74 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+class Node {
+public:
+    int data;
+    Node* next;
+
+    // Constructor
+    Node(int data1, Node* next1) {
+        data = data1;
+        next = next1;
+    }
+
+    // Constructor 
+    Node(int data1) {
+        data = data1;
+        next = nullptr;
+    }
+};
+
+// Function to print the linked list
+void printLL(Node* head) {
+    while (head != NULL) {
+        cout << head->data << " ";
+        head = head->next;
+    }
+}
+
+// Function to delete the Nth node
+// from the end of the linked list
+Node* DeleteNthNodefromEnd(Node* head, int N) {
+    // Create two pointers, fastp and slowp
+    Node* fastp = head;
+    Node* slowp = head;
+
+    // Move the fastp pointer N nodes ahead
+    for (int i = 0; i < N; i++)
+        fastp = fastp->next;
+
+    // If fastp becomes NULL,
+    // the Nth node from the end is the head
+    if (fastp == NULL)
+        return head->next;
+
+    // Move both pointers until fastp reaches the end
+    while (fastp->next != NULL) {
+        fastp = fastp->next;
+        slowp = slowp->next;
+    }
+
+    // Delete the Nth node from the end
+    Node* delNode = slowp->next;
+    slowp->next = slowp->next->next;
+    delete delNode;
+    return head;
+}
+
+int main() {
+    vector<int> arr = {1, 2, 3, 4, 5};
+    int N = 3;
+    Node* head = new Node(arr[0]);
+    head->next = new Node(arr[1]);
+    head->next->next = new Node(arr[2]);
+    head->next->next->next = new Node(arr[3]);
+    head->next->next->next->next = new Node(arr[4]);
+
+    // Delete the Nth node from the end 
+    // and print the modified linked list
+    head = DeleteNthNodefromEnd(head, N);
+    printLL(head);
+}
+
