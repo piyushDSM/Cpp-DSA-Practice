2d3569db67acf43369d6726444e8b0b5eb4748ad
piyushDSM <piyushchaubeydsm@gmail.com>
2024-04-14 13:04:33 +0530

    uPDATE

--- LinkedList/DetectCycleInLL.cpp
+++ LinkedList/DetectCycleInLL.cpp
@@ -6,24 +6,27 @@ using namespace std;
 
 // Node class represents a
 // node in a linked list
-class Node {
+class Node
+{
 public:
     // Data stored in the node
-    int data;        
-    
-    // Pointer to the next node in the list
-    Node* next;      
+    int data;
 
-    // Constructor with both data 
+    // Pointer to the next node in the list
+    Node *next;
+
+    // Constructor with both data
     // and next node as parameters
-    Node(int data1, Node* next1) {
+    Node(int data1, Node *next1)
+    {
         data = data1;
         next = next1;
     }
 
     // Constructor with only data as
     // a parameter, sets next to nullptr
-    Node(int data1) {
+    Node(int data1)
+    {
         data = data1;
         next = nullptr;
     }
@@ -31,23 +34,26 @@ public:
 
 // Function to detect a loop in a linked
 // list using the Tortoise and Hare Algorithm
-bool detectCycle(Node* head) {
+bool detectCycle(Node *head)
+{
     // Initialize two pointers, slow and fast,
     // to the head of the linked list
-    Node* slow = head;
-    Node* fast = head;
+    Node *slow = head;
+    Node *fast = head;
 
     // Step 2: Traverse the linked list with
     // the slow and fast pointers
-    while (fast != nullptr && fast->next != nullptr) {
+    while (fast != nullptr && fast->next != nullptr)
+    {
         // Move slow one step
         slow = slow->next;
         // Move fast two steps
         fast = fast->next->next;
 
         // Check if slow and fast pointers meet
-        if (slow == fast) {
-            return true;  // Loop detected
+        if (slow == fast)
+        {
+            return true; // Loop detected
         }
     }
 
@@ -56,29 +62,32 @@ bool detectCycle(Node* head) {
     return false;
 }
 
-
-int main() {
+int main()
+{
     // Create a sample linked list
     // with a loop for testing
-    
-    Node* head = new Node(1);
-    Node* second = new Node(2);
-    Node* third = new Node(3);
-    Node* fourth = new Node(4);
-    Node* fifth = new Node(5);
+
+    Node *head = new Node(1);
+    Node *second = new Node(2);
+    Node *third = new Node(3);
+    Node *fourth = new Node(4);
+    Node *fifth = new Node(5);
 
     head->next = second;
     second->next = third;
     third->next = fourth;
     fourth->next = fifth;
-     // Create a loop
-    fifth->next = third; 
+    // Create a loop
+    fifth->next = third;
 
-    // Check if there is a loop 
+    // Check if there is a loop
     // n the linked list
-    if (detectCycle(head)) {
+    if (detectCycle(head))
+    {
         cout << "Loop detected in the linked list." << endl;
-    } else {
+    }
+    else
+    {
         cout << "No loop detected in the linked list." << endl;
     }
 
@@ -91,4 +100,3 @@ int main() {
 
     return 0;
 }
-
