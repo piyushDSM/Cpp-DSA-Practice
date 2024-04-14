b0e1388234c812db5f7a2babfff0286e2b213f92
piyushDSM <piyushchaubeydsm@gmail.com>
2024-04-14 13:04:09 +0530

    uPDATE

--- LinkedList/DeleteNodeInLL.cpp
+++ LinkedList/DeleteNodeInLL.cpp
@@ -1,62 +1,73 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-class node {
-    public:
-        int num;
-        node* next;
-        node(int a) {
-            num = a;
-            next = NULL;
-        }
+class node
+{
+public:
+    int num;
+    node *next;
+    node(int a)
+    {
+        num = a;
+        next = NULL;
+    }
 };
-//function to insert node at the end of the list
-void insertNode(node* &head,int val) {
-    node* newNode = new node(val);
-    if(head == NULL) {
+// function to insert node at the end of the list
+void insertNode(node *&head, int val)
+{
+    node *newNode = new node(val);
+    if (head == NULL)
+    {
         head = newNode;
         return;
     }
-    node* temp = head;
-    while(temp->next != NULL) temp = temp->next;
+    node *temp = head;
+    while (temp->next != NULL)
+        temp = temp->next;
     temp->next = newNode;
 }
-//function to get reference of the node to delete
-node* getNode(node* head,int val) {
-    while(head->num != val) head = head->next;
-    
+// function to get reference of the node to delete
+node *getNode(node *head, int val)
+{
+    while (head->num != val)
+        head = head->next;
+
     return head;
 }
-//delete function as per the question
-void deleteNode(node* t) {
+// delete function as per the question
+void deleteNode(node *t)
+{
     t->num = t->next->num;
     t->next = t->next->next;
     return;
 }
-//printing the list function
-void printList(node* head) {
-    while(head->next != NULL) {
-        cout<<head->num<<"->";
+// printing the list function
+void printList(node *head)
+{
+    while (head->next != NULL)
+    {
+        cout << head->num << "->";
         head = head->next;
     }
-    cout<<head->num<<"\n";
+    cout << head->num << "\n";
 }
 
-int main() {
-    node* head = NULL;
-    //inserting node
-    insertNode(head,1);
-    insertNode(head,4);
-    insertNode(head,2);
-    insertNode(head,3);
-    //printing given list
-    cout<<"Given Linked List:\n";
+int main()
+{
+    node *head = NULL;
+    // inserting node
+    insertNode(head, 1);
+    insertNode(head, 4);
+    insertNode(head, 2);
+    insertNode(head, 3);
+    // printing given list
+    cout << "Given Linked List:\n";
     printList(head);
-    node* t = getNode(head,2);
-    //delete node
+    node *t = getNode(head, 2);
+    // delete node
     deleteNode(t);
-    //list after deletion operation
-    cout<<"Linked List after deletion:\n";
+    // list after deletion operation
+    cout << "Linked List after deletion:\n";
     printList(head);
     return 0;
 }
