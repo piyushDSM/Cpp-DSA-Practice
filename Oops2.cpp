aac5804dad1cbf63356705f3fb4439edddef9534
piyushDSM <piyushchaubeydsm@gmail.com>
2024-05-20 12:55:29 +0530

    Practiced_OOPS

--- Oops2.cpp
+++ Oops2.cpp
@@ -1,10 +1,51 @@
-#include<bits/stdc++.h>
-#include<iostream>
+#include <bits/stdc++.h>
+#include <iostream>
 using namespace std;
+
+class Human
+{
+public:
+    int age;
+    int height;
+    int weight;
+
+public:
+    int getAge(int b)
+    {
+        return this->age = b;
+    }
+    void setAge( int a){
+        this-> age = a;
+    }
+    void setweight(int w)
+    {
+        this->weight = w;
+
+    }
+};
+
+class Male : public Human
+{
+
+public:
+    string color;
+
+    void sleeping()
+    {
+        cout << "male is sleeping";
+    }
+};
 
 int main()
 {
+    Male obj1;
+    
+    obj1.getAge(22);
+    cout<< obj1.age <<endl;
+    cout<< obj1.height<<endl;
+    cout<< obj1.weight<<endl;
+    obj1.sleeping();
 
-
-return 0;
+    
+    return 0;
 }
