f386bf7ca9249cb9e1f75b3e5db0c3ce55af32ab
piyushDSM <piyushchaubeydsm@gmail.com>
2024-05-20 16:10:23 +0530

    OPPS-Ambiguity-Practice

--- Oops2.cpp
+++ Oops2.cpp
@@ -1,51 +1,86 @@
-#include <bits/stdc++.h>
-#include <iostream>
+// #include <bits/stdc++.h>
+// #include <iostream>
+// using namespace std;
+
+// class Human
+// {
+// public:
+//     int age;
+//     int height;
+//     int weight;
+
+// public:
+//     int getAge(int b)
+//     {
+//         return this->age = b;
+//     }
+//     void setAge( int a){
+//         this-> age = a;
+//     }
+//     void setweight(int w)
+//     {
+//         this->weight = w;
+
+//     }
+// };
+
+// class Male : public Human
+// {
+
+// public:
+//     string color;
+
+//     void sleeping()
+//     {
+//         cout << "male is sleeping";
+//     }
+// };
+
+// int main()
+// {
+//     Male obj1;
+    
+//     obj1.getAge(22);
+//     cout<< obj1.age <<endl;
+//     cout<< obj1.height<<endl;
+//     cout<< obj1.weight<<endl;
+//     obj1.sleeping();
+
+    
+//     return 0;
+// 
+
+
+/* Ambiguity inheritance */
+#include<bits/stdc++.h>
+#include<iostream>
 using namespace std;
 
-class Human
-{
-public:
-    int age;
-    int height;
-    int weight;
 
+class A {
 public:
-    int getAge(int b)
-    {
-        return this->age = b;
-    }
-    void setAge( int a){
-        this-> age = a;
-    }
-    void setweight(int w)
-    {
-        this->weight = w;
-
-    }
+   void fnc(){
+    cout<< "Hello From A";
+   }
+};
+class B {
+    public:
+   void fnc(){
+    cout<< "Hello From B";
+   }
 };
 
-class Male : public Human
-{
+class C : public A , public B {
 
-public:
-    string color;
-
-    void sleeping()
-    {
-        cout << "male is sleeping";
-    }
 };
 
 int main()
 {
-    Male obj1;
-    
-    obj1.getAge(22);
-    cout<< obj1.age <<endl;
-    cout<< obj1.height<<endl;
-    cout<< obj1.weight<<endl;
-    obj1.sleeping();
 
-    
-    return 0;
+C obj;
+obj.A::fnc(); 
+cout<<endl;
+obj.B::fnc();  
+
+return 0;
 }
