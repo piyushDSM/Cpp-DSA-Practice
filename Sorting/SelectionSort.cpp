371aaffd52873b0163d0539da16c71f3c44c4ff4
piyushDSM <piyushchaubeydsm@gmail.com>
2024-05-24 17:13:08 +0530

    Updated

--- Sorting/SelectionSort.cpp
+++ Sorting/SelectionSort.cpp
@@ -1,35 +1,40 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 
 using namespace std;
 
-void selection_sort(int arr[],int n ){
-     for(int i = 0; i<=n-2; i++){
+void selection_sort(int arr[], int n)
+{
+    for (int i = 0; i <= n - 2; i++)
+    {
         int mini = i;
-        for(int j = i; j<=n-1; j++){
-            if(arr[j] < arr[mini]){
+        for (int j = i; j <= n - 1; j++)
+        {
+            if (arr[j] < arr[mini])
+            {
                 mini = j;
             }
         }
         int temp = arr[mini];
         arr[mini] = arr[i];
         arr[i] = temp;
-     }
+    }
 }
 
 int main()
 {
 
-int n;
-cin>>n;
+    int n;
+    cin >> n;
 
-int arr[n];
-for(int i = 0; i<n; i++)
-cin>>arr[i];
+    int arr[n];
+    for (int i = 0; i < n; i++)
+        cin >> arr[i];
 
-selection_sort(arr,n);
-for(int i = 0; i<n;i++){
-    cout<<arr[i] << " ";
-}
+    selection_sort(arr, n);
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
 
-return 0;
+    return 0;
 }
