dbd6156a434771f253b3556c171a5ab8f733c640
piyushDSM <piyushchaubeydsm@gmail.com>
2024-07-16 12:46:50 +0530

    Revised-pointer

---
+++ pointer.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+#include<iostream>
+using namespace std;
+
+int main()
+{
+
+int x = 5;
+int *ptr = &x;
+
+cout<<ptr<<endl;
+cout<<*ptr<<endl;
+cout<<x<<endl;
+
+
+return 0;
+}
