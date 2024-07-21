f73fe8444cb2b54a6cbe70148542b2dbfa5db549
piyushDSM <piyushchaubeydsm@gmail.com>
2024-07-21 12:22:32 +0530

    Updated

--- linkedlist.cpp
+++ linkedlist.cpp
@@ -1,53 +1,54 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-class Node {
+class Node
+{
 
-    public:
+public:
     int data;
     Node *next;
-    
-    //Constructor
+
+    // Constructor
     Node(int data)
     {
-        this -> data = data;
-        this -> next = NULL;
+        this->data = data;
+        this->next = NULL;
     }
 };
 
-void insertAtHead(Node* &head, int d)
+void insertAtHead(Node *&head, int d)
 {
-    //new node create
+    // new node create
     Node *temp = new Node(d);
-    temp -> next = head;
+    temp->next = head;
     head = temp;
 }
 
-void print(Node* &head){
-     Node *temp = head;
+void print(Node *&head)
+{
+    Node *temp = head;
 
-     while(temp !=NULL)
+    while (temp != NULL)
     {
-      cout<< temp -> data << " ";
-      cout<< temp -> next;
+        cout << temp->data << " ";
+        cout << temp->next;
     }
-    cout<< endl;
+    cout << endl;
 }
-
 
 int main()
 {
 
-// creation of new node
-Node *node1 = new Node(10);
-// cout<< node1 -> data << endl;
-// cout<< node1 -> next << endl;
+    // creation of new node
+    Node *node1 = new Node(10);
+    // cout<< node1 -> data << endl;
+    // cout<< node1 -> next << endl;
 
-// head pointed to node1
-Node* head =node1;
-print(head);
-insertAtHead(head,12);
-print(head);
+    // head pointed to node1
+    Node *head = node1;
+    print(head);
+    insertAtHead(head, 12);
+    print(head);
 
-return 0;
+    return 0;
 }
