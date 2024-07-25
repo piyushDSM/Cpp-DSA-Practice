b2b9ac7edeffe4738c378c1e7c4a53260644a217
piyushDSM <piyushchaubeydsm@gmail.com>
2024-07-25 11:40:59 +0530

    Updated-Notes-POINTER

--- PRACTICE/POINTER.cpp
+++ PRACTICE/POINTER.cpp
@@ -5,12 +5,31 @@ using namespace std;
 int main()
 {
 
-int x = 20;
-int *ptr;
-ptr =&x; // Or  int *ptr = &x
+// int x = 20;
+// int *ptr;
+// ptr =&x; // Or  int *ptr = &x
 
-cout<<ptr<<endl; // ADDRESS
-cout<<*ptr; //VALUE
+// cout<<ptr<<endl; // ADDRESS
+// cout<<*ptr; //VALUE
+
+int i =5;
+int *p = &i;
+int **p2 = &p;
+
+cout<< i<<endl;  // value of i 
+cout<<*p<<endl;
+cout<<**p2<<endl;
+
+cout<<endl;
+
+cout<<&i<<endl;  // address of i
+cout<<p<<endl;
+cout<<*p2<<endl;
+
+cout<<endl;
+
+cout<<&p<<endl;  // 
+cout<<&p2;
 
 return 0;
 }
