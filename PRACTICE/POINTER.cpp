c66be3a3a487b220b4c20882a5cab0e94885e813
piyushDSM <piyushchaubeydsm@gmail.com>
2024-08-04 16:09:21 +0530

    Updated

--- PRACTICE/POINTER.cpp
+++ PRACTICE/POINTER.cpp
@@ -1,35 +1,35 @@
-#include<bits/stdc++.h>
-#include<iostream>
+#include <bits/stdc++.h>
+#include <iostream>
 using namespace std;
 
 int main()
 {
 
-// int x = 20;
-// int *ptr;
-// ptr =&x; // Or  int *ptr = &x
+    // int x = 20;
+    // int *ptr;
+    // ptr =&x; // Or  int *ptr = &x
 
-// cout<<ptr<<endl; // ADDRESS
-// cout<<*ptr; //VALUE
+    // cout<<ptr<<endl; // ADDRESS
+    // cout<<*ptr; //VALUE
 
-int i =5;
-int *p = &i;
-int **p2 = &p;
+    int i = 5;
+    int *p = &i;
+    int **p2 = &p;
 
-cout<< i<<endl;  // value of i 
-cout<<*p<<endl;
-cout<<**p2<<endl;
+    cout << i << endl; // value of i
+    cout << *p << endl;
+    cout << **p2 << endl;
 
-cout<<endl;
+    cout << endl;
 
-cout<<&i<<endl;  // address of i
-cout<<p<<endl;
-cout<<*p2<<endl;
+    cout << &i << endl; // address of i
+    cout << p << endl;
+    cout << *p2 << endl;
 
-cout<<endl;
+    cout << endl;
 
-cout<<&p<<endl;  // address of p
-cout<<p2;
+    cout << &p << endl; // address of p
+    cout << p2;
 
-return 0;
+    return 0;
 }
