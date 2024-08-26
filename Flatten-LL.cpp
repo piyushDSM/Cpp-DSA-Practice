7965af4ac99efef2466eb17e54bcc5608cedc428
piyushDSM <piyushchaubeydsm@gmail.com>
2024-08-26 16:40:44 +0530

    Updated

--- Flatten-LL.cpp
+++ Flatten-LL.cpp
@@ -1,8 +1,9 @@
-                                
+
 #include <iostream>
 using namespace std;
 
-class Node {
+class Node
+{
 public:
     int data;
     Node *next;
@@ -17,20 +18,24 @@ public:
 
 // Merges two linked lists in a particular
 // order based on the data value
-Node* merge(Node* list1, Node* list2){
+Node *merge(Node *list1, Node *list2)
+{
     // Create a dummy node as a
     // placeholder for the result
-    Node* dummyNode = new Node(-1);
-    Node* res = dummyNode;
+    Node *dummyNode = new Node(-1);
+    Node *res = dummyNode;
 
     // Merge the lists based on data values
-    while(list1 != NULL && list2 != NULL){
-        if(list1->data < list2->data){
+    while (list1 != NULL && list2 != NULL)
+    {
+        if (list1->data < list2->data)
+        {
             res->child = list1;
             res = list1;
             list1 = list1->child;
         }
-        else{
+        else
+        {
             res->child = list2;
             res = list2;
             list2 = list2->child;
@@ -40,15 +45,19 @@ Node* merge(Node* list1, Node* list2){
 
     // Connect the remaining
     // elements if any
-    if(list1){
+    if (list1)
+    {
         res->child = list1;
-    } else {
+    }
+    else
+    {
         res->child = list2;
     }
 
     // Break the last node's
     // link to prevent cycles
-    if(dummyNode->child){
+    if (dummyNode->child)
+    {
         dummyNode->child->next = NULL;
     }
 
@@ -56,24 +65,28 @@ Node* merge(Node* list1, Node* list2){
 }
 
 // Flattens a linked list with child pointers
-Node* flattenLinkedList(Node* head){
-    // If head is null or there 
+Node *flattenLinkedList(Node *head)
+{
+    // If head is null or there
     // is no next node, return head
-    if(head == NULL || head->next == NULL){
+    if (head == NULL || head->next == NULL)
+    {
         return head;
     }
 
     // Recursively flatten the
     // rest of the linked list
-    Node* mergedHead = flattenLinkedList(head->next);
+    Node *mergedHead = flattenLinkedList(head->next);
     head = merge(head, mergedHead);
     return head;
 }
 
 // Print the linked list by
 // traversing through child pointers
-void printLinkedList(Node* head) {
-    while (head != nullptr) {
+void printLinkedList(Node *head)
+{
+    while (head != nullptr)
+    {
         cout << head->data << " ";
         head = head->child;
     }
@@ -82,22 +95,27 @@ void printLinkedList(Node* head) {
 
 // Print the linked list
 // in a grid-like structure
-void printOriginalLinkedList(Node* head, int depth) {
-    while (head != nullptr) {
+void printOriginalLinkedList(Node *head, int depth)
+{
+    while (head != nullptr)
+    {
         cout << head->data;
 
         // If child exists, recursively
         // print it with indentation
-        if (head->child) {
+        if (head->child)
+        {
             cout << " -> ";
             printOriginalLinkedList(head->child, depth + 1);
         }
 
         // Add vertical bars
         // for each level in the grid
-        if (head->next) {
+        if (head->next)
+        {
             cout << endl;
-            for (int i = 0; i < depth; ++i) {
+            for (int i = 0; i < depth; ++i)
+            {
                 cout << "| ";
             }
         }
@@ -105,18 +123,19 @@ void printOriginalLinkedList(Node* head, int depth) {
     }
 }
 
-int main() {
+int main()
+{
     // Create a linked list with child pointers
-    Node* head = new Node(5);
+    Node *head = new Node(5);
     head->child = new Node(14);
-    
+
     head->next = new Node(10);
     head->next->child = new Node(4);
-    
+
     head->next->next = new Node(12);
     head->next->next->child = new Node(20);
     head->next->next->child->child = new Node(13);
-    
+
     head->next->next->next = new Node(7);
     head->next->next->next->child = new Node(17);
 
@@ -127,12 +146,9 @@ int main() {
 
     // Flatten the linked list
     // and print the flattened list
-    Node* flattened = flattenLinkedList(head);
+    Node *flattened = flattenLinkedList(head);
     cout << "\nFlattened linked list: ";
     printLinkedList(flattened);
 
     return 0;
 }
-
-                                
-                            
