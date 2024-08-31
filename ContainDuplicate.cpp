e4a0fbad57520b137d5c0e650627e6f5fcfcf59d
piyushDSM <piyushchaubeydsm@gmail.com>
2024-08-31 15:39:20 +0530

    Format

--- ContainDuplicate.cpp
+++ ContainDuplicate.cpp
@@ -1,11 +1,15 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-    
-bool containsDuplicate(vector<int> nums) {
- 
-    for(int i = 0; i < nums.size(); i++) {
-        for(int j = i + 1; j < nums.size(); j++) {
-            if(nums[i] == nums[j]) {
+
+bool containsDuplicate(vector<int> nums)
+{
+
+    for (int i = 0; i < nums.size(); i++)
+    {
+        for (int j = i + 1; j < nums.size(); j++)
+        {
+            if (nums[i] == nums[j])
+            {
                 return true;
             }
         }
@@ -13,16 +17,17 @@ bool containsDuplicate(vector<int> nums) {
 
     return false;
 }
-    
-int main () {
-	    
-    vector<int> nums {1, 2, 3, 1};
-	    
+
+int main()
+{
+
+    vector<int> nums{1, 2, 3, 1};
+
     bool res = containsDuplicate(nums);
-	    
+
     // printing the result
-    if(res== 1)
-        cout<<"true"<<endl;
+    if (res == 1)
+        cout << "true" << endl;
     else
-        cout<<"false"<<endl;
+        cout << "false" << endl;
 }
